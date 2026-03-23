f174dc58d3805595d27d7526a3b30dbb6b707f45
Akalya R <akalyaramamoorthi20@gmail.com>
2026-03-23 13:30:01 +0530

    Time: 382 ms (17.09%), Space: 42.8 MB (64.84%) - LeetHub

---
+++ 0037-sudoku-solver/0037-sudoku-solver.java
@@ -0,0 +1,45 @@
+class Solution {
+    public void solveSudoku(char[][] board) {
+        solve(board);
+    }
+
+    private boolean solve(char[][] board) {
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+
+                if (board[i][j] == '.') {
+                    for (char c = '1'; c <= '9'; c++) {
+
+                        if (isValid(board, i, j, c)) {
+                            board[i][j] = c;
+
+                            if (solve(board)) return true;
+
+                            board[i][j] = '.'; // backtrack
+                        }
+                    }
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private boolean isValid(char[][] board, int row, int col, char c) {
+
+        for (int i = 0; i < 9; i++) {
+            // row check
+            if (board[row][i] == c) return false;
+
+            // column check
+            if (board[i][col] == c) return false;
+
+            // 3x3 check
+            int r = 3 * (row / 3) + i / 3;
+            int cl = 3 * (col / 3) + i % 3;
+            if (board[r][cl] == c) return false;
+        }
+
+        return true;
+    }
+}
